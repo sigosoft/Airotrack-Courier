a5fc12492564b8acae8cd5424b97856e49d6de12
Stephy <stephy.sigosoft@gmail.com>
2026-05-28 10:12:46 +0530

    commit after barcode scan bug fix on 28/05/2026

--- lib/controllers/scan_device_controller.dart
+++ lib/controllers/scan_device_controller.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:dio/dio.dart';
 import '../services/api_service.dart';
 import 'home_controller.dart';
 
@@ -246,9 +247,30 @@ class ScanDeviceController extends GetxController {
     } catch (e) {
       _scannedBarcodes.remove(imei);
       debugPrint("Scan API Error: $e");
+      String errorMessage = "Scan API Error: $e";
+      if (e is DioException) {
+        var data = e.response?.data;
+        if (data != null && data is Map && data['message'] != null) {
+          var msg = data['message'];
+          if (msg is String) {
+            errorMessage = msg;
+          } else if (msg is Map && msg.isNotEmpty) {
+            var firstError = msg.values.first;
+            errorMessage = (firstError is List && firstError.isNotEmpty)
+                ? firstError.first.toString()
+                : firstError.toString();
+          } else {
+            errorMessage = msg.toString();
+          }
+        } else {
+          errorMessage =
+              e.message ??
+              "Server error (${e.response?.statusCode ?? 'unknown'})";
+        }
+      }
       Get.snackbar(
         "Error",
-        "Scan API Error: $e",
+        errorMessage,
         snackPosition: SnackPosition.BOTTOM,
         backgroundColor: Colors.red,
         colorText: Colors.white,
